038dc483eabfe649f45942297abc50a423cd2a56
erik-kostanyan <erik.kostanyan@hotmail.com>
2025-01-26 23:20:08 +0100

    header update

--- src/app/page.js
+++ src/app/page.js
@@ -51,10 +51,6 @@ export default async function Home() {
   }
   return (
     <div>
-      <img
-        src='https://firebasestorage.googleapis.com/v0/b/mern-auth-1c4ae.appspot.com/o/1693307829089home%203.jpeg?alt=media&token=8dcc9a22-a8d3-4737-b27f-7c77b417a7d0'
-        className='w-full h-[550px] object-cover'
-      />
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
